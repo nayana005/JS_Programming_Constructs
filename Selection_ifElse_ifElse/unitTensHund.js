8019a479f6d35fd84013b93b4b6db4ae1790c3a3
nayana005 <nayanasuresh1005@gmail.com>
2022-10-10 15:53:21 +0530

    Selection if Else if Statement

---
+++ Selection_ifElse_ifElse/unitTensHund.js
@@ -0,0 +1,20 @@
+var num = 10;
+
+if ( num == 1 ) {
+    console.log("Units");
+}
+else if ( num == 10 ) {
+    console.log("Tens");
+}
+else if ( num == 100 ) {
+    console.log("Hundreds");
+}
+else if ( num == 1000 ) {
+    console.log("Thousands");
+}
+else if ( num == 10000 ) {
+    console.log("Ten thousands");
+}
+else {
+    console.log("Invalid num");
+}
